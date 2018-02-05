1a645e62ea40dfad2047145e508ddc8630c718df
lg930720 <340388720@qq.com>
2018-02-05 17:30:57 +0800

    使用VUE的渲染函数

--- my-project/src/main.js
+++ my-project/src/main.js
@@ -7,11 +7,17 @@ import ElementUI from 'element-ui'
 import 'element-ui/lib/theme-chalk/index.css'
 
 Vue.config.productionTip = false
+Vue.use(ElementUI)
 
 /* eslint-disable no-new */
+// new Vue({
+//   el: '#app',
+//   router,
+//   components: { App },
+//   template: '<App/>'
+// })
 new Vue({
   el: '#app',
   router,
-  components: { App },
-  template: '<App/>'
+  render: h => h(App)
 })
